6548134c5937a7a24908653b2a2ebb6cd64b3943
Martin Han <martin.m.han@gmail.com>
2020-09-21 20:58:56 -0700

    fix lint issues in test file

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,11 +1,11 @@
-import request from 'supertest'
+import request from 'supertest';
 
-import app from '../src/app'
+import app from '../src/app';
 
 describe('GET /api/test', () => {
   it('should return 200 OK', async () => {
-    const response = await request(app).get('/api/test')
-    expect(response.status).toBe(200)
-    expect (response.body).toEqual({ someData: 'hello world' })
-  })
-})
+    const response = await request(app).get('/api/test');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ someData: 'hello world' });
+  });
+});
