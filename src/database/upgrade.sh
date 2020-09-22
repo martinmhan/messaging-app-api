e13f358858e21c468dc11211edb9569dcd01d55d
Martin Han <martin.m.han@gmail.com>
2020-09-22 15:59:17 -0700

    add new db script and upgrade sh file, rename 0- db script

---
+++ src/database/upgrade.sh
@@ -0,0 +1,4 @@
+# Run this script to run all the SQL scripts in the /scripts folder sequentially
+# SQL scripts should be idempotent, so running this script multiple times should not cause any problems
+
+# TO DO - write this script
