e3ab22bb76302bc4d1eaa1427336dce6e0636fa6
Martin Han <martin.m.han@gmail.com>
2020-09-24 17:41:05 -0700

    flesh out db upgrade.sh

--- src/database/upgrade.sh
+++ src/database/upgrade.sh
@@ -1,4 +1,18 @@
-# Run this script to run all the SQL scripts in the /scripts folder sequentially
-# SQL scripts should be idempotent, so running this script multiple times should not cause any problems
+# Run this script to run all the SQL scripts in the /scripts folder in order
+# SQL scripts should be labeled in order (with the appropriate leading 0s)
+# SQL scripts should be idempotent, i.e., running all of them in order multiple times should not cause any problems.
+# Changes to the database schema should only be done via this script
 
-# TO DO - write this script
+set -e
+
+source ../../.env;
+
+cd scripts
+
+for FILE in `ls *.sql | sort -V`; do
+  mysql -h $DB_HOST -u $DB_USER --password=$DB_PASS $DB_NAME < $FILE;
+  echo "Ran $FILE";
+done;
+
+echo "Database updated successfully";
+exit 0;
