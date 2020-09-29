7e0bb60d64f78dc1306e8af641a935bab2420db7
Martin Han <martin.m.han@gmail.com>
2020-09-29 11:26:08 -0700

    add conversation api tests placeholder file

---
+++ test/api/conversation.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+import dotenv from 'dotenv';
+import * as uuid from 'uuid';
+
+dotenv.config();
+
+import app from '../../src/app';
+import User from '../../src/models/User';
+import Conversation from '../../src/models/Conversation';
+import MySQLDatabaseAccess from '../../src/database/MySQLDatabaseAccess';
+
+jest.mock('../../src/database/MySQLDatabaseAccess.ts'); // comment this line to use the real database for tests
+   
